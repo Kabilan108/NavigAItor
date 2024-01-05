6f5a2045c3bfad846b11d661cd2f489f4070c4dc
Tony Okeke <tonykabilanokeke@gmail.com>
2024-01-04 19:13:25 -0500

    feat: add script to run fastapi server

---
+++ run_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# install poetry
+pip3 install poetry
+
+# install dependencies
+poetry shell
+poetry install
+
+# run server
+poetry run python -m uvicorn api.index:app \
+    --host $API_HOST \
+    --port $API_PORT \
+    --reload
