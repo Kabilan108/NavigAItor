1dce5ef8f018a371d6a45b62476b08d923c75de8
Tony Okeke <tonykabilanokeke@gmail.com>
2024-01-07 05:09:12 -0500

    feat: set up shadcn

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -40,6 +42,10 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+
+      <div>
+        <Button>Click me</Button>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
