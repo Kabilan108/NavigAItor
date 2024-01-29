70d26b89d995014031408a57c78b9011d5aed806
Tony Okeke <tonykabilanokeke@gmail.com>
2024-01-29 15:07:16 -0500

    build: isolate scripts

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-# Activate Poetry environment
+cd "$(dirname "$0")/../backend"
+
 source "$(poetry env info --path)/bin/activate"
 
-# Install dependencies
 poetry install --no-root
 
-# Run server
 poetry run python -m uvicorn api.index:app \
     --host $API_HOST \
     --port $API_PORT \
