5aae44c01432407c404c1c5e8a6b9d543f4b781a
Tony Okeke <tonykabilanokeke@gmail.com>
2024-01-30 03:47:35 -0500

    feat(backend): add /transcribe and /status endpoints

--- backend/services/api.py
+++ backend/services/api.py
@@ -3,24 +3,45 @@
 # API server running serverless with Modal.
 # Provides endpoints for GPU inference of different models.
 
-from fastapi import FastAPI, Depends, Request, HTTPException, status
+from fastapi import (
+    Depends,
+    File,
+    FastAPI,
+    HTTPException,
+    UploadFile,
+    status,
+)
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from fastapi.responses import JSONResponse
 
-from modal.functions import FunctionCall
+from typing import NamedTuple
+import time
+import os
 
-from .main import slow_operation
+from .main import data_volume, transcribe, stub
+from .config import logger
+from . import utils
 
+
+MAX_JOB_AGE_SECS = 10 * 60
 
 app = FastAPI()
 auth_scheme = HTTPBearer()
 
 
-@app.post("/accept")
-async def accept_json(
-    request: Request, token: HTTPAuthorizationCredentials = Depends(auth_scheme)
+class RunningJob(NamedTuple):
+    call_id: str
+    start_time: int
+
+
+@app.post("/transcribe")
+async def transcribe_job(
+    file: UploadFile = File(...),
+    token: HTTPAuthorizationCredentials = Depends(auth_scheme),
 ):
-    import os
+    """Start transcription jobs"""
+
+    path, filename = utils.save_file(file)
+    data_volume.commit()
 
     if token.credentials != os.environ["ACCESS_TOKEN"]:
         raise HTTPException(
@@ -29,19 +50,60 @@ async def accept_json(
             headers={"WWW-Authenticate": "Bearer"},
         )
 
-    call = slow_operation.spawn()
+    now = int(time.time())
 
-    return JSONResponse(
-        {"call_id": call.object_id},
-        status_code=status.HTTP_200_OK,
+    try:
+        job = stub.jobs[filename]
+
+        if isinstance(job, RunningJob) and (now - job.start_time) < MAX_JOB_AGE_SECS:
+            call_id = job.call_id
+            utils.logger.info(
+                f"Found existing, unexpired job for {filename}. Returning call_id {call_id}."
+            )
+            return {"call_id": call_id}
+    except KeyError:
+        pass
+
+    call = transcribe.spawn(audiofile=path)
+    stub.jobs[filename] = RunningJob(
+        call_id=call.object_id,
+        start_time=now,
     )
+
+    return {"call_id": call.object_id}
 
 
 @app.get("/status/{call_id}")
 async def poll_status(call_id: str):
-    call = FunctionCall.from_id(call_id)
+    """Check status of a running job"""
+
+    from modal.functions import FunctionCall
+
+    function_call = FunctionCall.from_id(call_id)
 
     try:
-        return call.get(timeout=0)
+        transcript = function_call.get(timeout=0.1)
     except TimeoutError:
-        return JSONResponse({}, status_code=status.HTTP_202_ACCEPTED)
+        return {"status": "running"}
+    except Exception as exc:
+        if exc.args:
+            inner_exc = exc.args[0]
+            if "HTTPError 403" in inner_exc:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail="Permission denied on transcription job",
+                )
+
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Unknown error processing job",
+        )
+
+    logger.info(f"Received segments for {call_id}")
+    logger.info(f"Segments: {transcript['text']}")
+
+    return {
+        "status": "complete",
+        "transcript": transcript["text"],
+        "timestamps": transcript["timestamps"],
+    }
