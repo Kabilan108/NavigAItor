c913c33dfbc2589547c36c538c28a38c22b3967e
Tony Okeke <tonykabilanokeke@gmail.com>
2024-05-09 01:57:02 -0400

    build: set up tailwindcss and shadcn-ui

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,7 +1,8 @@
 function LandingPage() {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold">Landing Page</h1>
+      <p className="text-gray-800">This is the landing page.</p>
     </div>
   );
 }
