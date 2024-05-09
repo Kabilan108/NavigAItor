6dab6e3c1b1e999e50ec54e6ccc11917058f49fe
Tony Okeke <tonykabilanokeke@gmail.com>
2024-05-09 17:58:44 -0400

    feat: add callback for login

--- frontend/src/pages/LogIn.tsx
+++ frontend/src/pages/LogIn.tsx
@@ -1,8 +1,21 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 
 export default function LoginPage() {
+  // handle google sign in
+  const handleGoogleSignIn = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/auth/login`,
+      );
+      console.log(`Login success: ${response.data}`);
+    } catch (error) {
+      console.error(`Login failed: ${error}`);
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background">
       <Card className="mx-auto flex flex-col rounded-lg p-4 shadow-lg w-96 mb-16">
@@ -13,11 +26,15 @@ export default function LoginPage() {
         <CardContent className="grid gap-4">
           <div className="grid gap-6">
             <Button variant="outline" className="w-full">
-              <Icons.everythingApp className="mr-2 h-5 w-5" />
+              <Icons.twitter className="mr-2 h-5 w-5" />
             </Button>
           </div>
           <div className="grid gap-6">
-            <Button variant="outline" className="w-full">
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={handleGoogleSignIn}
+            >
               <Icons.google className="mr-2 h-5 w-5" />
             </Button>
           </div>
