63a6905632e5d1c45a3539ab415f97ff0d071853
Tony Okeke <tonykabilanokeke@gmail.com>
2024-05-10 06:39:37 -0400

    feat: add script to start api

---
+++ backend/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export APP_MODULE=${APP_MODULE-main:app}
+export HOST=${HOST-0.0.0.0}
+export PORT=${PORT-8000}
+
+# source .venv/bin/activate
+exec gunicorn "$APP_MODULE" \
+    --bind "$HOST":"$PORT" \
+    -k uvicorn.workers.UvicornWorker \
+    # --workers 2 \
+    --reload
