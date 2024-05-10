52a4967f0e3f58b326e1ef68bc81917a66abf729
Tony Okeke <tonykabilanokeke@gmail.com>
2024-05-10 08:37:52 -0400

    feat: implement db management for users

--- backend/api/endpoints/auth.py
+++ backend/api/endpoints/auth.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Request, HTTPException, Depends
 from authlib.integrations.starlette_client import OAuthError
 from fastapi.responses import RedirectResponse
 
-from api.deps import get_settings, Settings
-from core.auth import oauth
+from api.deps import get_db, get_settings, AsyncIOMotorClient, Settings
+from schema.auth import OAuthUser, OAuthUserInDB
+from core.auth import oauth, get_or_create_user
 
 router = APIRouter()
 
@@ -15,14 +16,20 @@ async def login(request: Request):
 
 
 @router.get("/token", name="token")
-async def auth(request: Request, settings: Settings = Depends(get_settings)):
+async def token(
+    request: Request,
+    settings: Settings = Depends(get_settings),
+    db: AsyncIOMotorClient = Depends(get_db),
+) -> RedirectResponse:
     try:
         token = await oauth.google.authorize_access_token(request)
     except OAuthError as e:
         raise HTTPException(status_code=400, detail=f"Failed to authenticate user: {e}")
-    user = token.get("userinfo")
-    if user:
-        request.session["user"] = user
+
+    user_info = OAuthUser(**token.get("userinfo"))
+    user: OAuthUserInDB = await get_or_create_user(db, user_info)
+    request.session["user"] = user.model_dump()
+
     return RedirectResponse(url=f"{settings.CLIENT_URL}/app")
 
 
--- backend/core/auth.py
+++ backend/core/auth.py
@@ -1,6 +1,10 @@
 from authlib.integrations.starlette_client import OAuth
 from starlette.config import Config
 
+from datetime import datetime
+
+from schema.auth import OAuthUser, OAuthUserInDB
+from api.deps import AsyncIOMotorClient
 from core.config import ROOT
 
 config = Config(ROOT / ".env")
@@ -12,3 +16,22 @@ oauth.register(
     server_metadata_url=CONF_URL,
     client_kwargs={"scope": "openid email profile"},
 )
+
+
+async def get_or_create_user(
+    db: AsyncIOMotorClient, user_info: OAuthUser
+) -> OAuthUserInDB:
+    user_data = await db.users.find_one({"email": user_info.email})
+    last_login = datetime.now()
+
+    if user_data:
+        await db.users.update_one(
+            {"email": user_info.email}, {"$set": {"last_login": last_login}}
+        )
+        user = OAuthUserInDB(**user_data)
+        user.last_login = last_login
+        return user
+    else:
+        user = OAuthUserInDB.from_oauth_user(user_info)
+        await db.users.insert_one(user.model_dump())
+        return user
